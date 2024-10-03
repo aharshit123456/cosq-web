86b5c0207c5174f3d8c4db903348c2c0ec7c3cac
Harshit Agarwal <aharshit123456@gmail.com>
2024-10-03 19:09:54 +0530

    no-reveal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React , { useState }from 'react';
 import './App.css'; // Import your CSS styles
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
+
+  const [showModal, setShowModal] = useState(false); // State to manage modal visibility
+
+  const handleButtonClick = (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+    setShowModal(true); // Show the modal
+  };
+
   return (
     <div className="container">
       <img src="icon.png" alt="Event Logo" className="logo" />
@@ -21,13 +29,29 @@ const App = () => {
       </div>
 
       <div className="buttons">
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdetxNNoUbwSHmtHJQ2oZELsQIkSAujYFkF61ab_jbFk_wcfw/viewform?usp=sf_link" className="button">
+        {/* <a href="https://docs.google.com/forms/d/e/1FAIpQLSdetxNNoUbwSHmtHJQ2oZELsQIkSAujYFkF61ab_jbFk_wcfw/viewform?usp=sf_link" className="button">
+          <i className="fas fa-user-friends"></i> Join Us Adventurers (Volunteers)
+        </a> */}
+        <a href="#" className="button" onClick={handleButtonClick}>
           <i className="fas fa-user-friends"></i> Join Us Adventurers (Volunteers)
         </a>
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeC3gq88aZioE1lyK7zwoY-pjrXa7qBVcNiO92kmVhSO19apQ/viewform?usp=sf_link" className="button">
+        {/* <a href="https://docs.google.com/forms/d/e/1FAIpQLSeC3gq88aZioE1lyK7zwoY-pjrXa7qBVcNiO92kmVhSO19apQ/viewform?usp=sf_link" className="button">
           <i className="fas fa-building"></i> Attention Business Owners
-        </a>
+        </a> */}
+                <a href="#" className="button" onClick={handleButtonClick}>
+                <i className="fas fa-building"></i> Attention Business Owners
+                </a>
       </div>
+
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Wait till tomorrow</p>
+            <button onClick={() => setShowModal(false)}>Close</button>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 };
